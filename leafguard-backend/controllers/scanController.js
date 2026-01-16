59b65e52b10c556a537ba6c4b8ac24120f851d04
winnerJd <jossyastu@gmail.com>
2026-01-16 21:08:12 +0300

    add  GetHistory  controller

--- leafguard-backend/controllers/scanController.js
+++ leafguard-backend/controllers/scanController.js
@@ -80,3 +80,26 @@ exports.uploadScan = async (req, res) => {
     if (client) client.release();
   }
 };
+
+// Get scan
+exports.getHistory = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const query = `
+      SELECT 
+        s.id, s.label, s.confidence, s.image_url, s.plant_status, s.created_at,
+        d.plant_name, d.cause, d.symptoms, d.treatment
+      FROM scans s
+      JOIN disease_info d ON s.label = d.label
+      WHERE s.user_id = $1
+      ORDER BY s.created_at DESC
+    `;
+    const result = await pool.query(query, [userId]);
+    res.status(200).json({ success: true, data: result.rows });
+  } catch (err) {
+    console.error(err.message);
+    res
+      .status(500)
+      .json({ success: false, error: "Server error fetching history" });
+  }
+};
