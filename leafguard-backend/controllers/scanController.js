9bfa1cdee69c2b308fd811632463e103986534ba
winnerJd <jossyastu@gmail.com>
2026-01-16 21:02:35 +0300

    add Scan validation schema  Scancontroller

--- leafguard-backend/controllers/scanController.js
+++ leafguard-backend/controllers/scanController.js
@@ -1,2 +1,11 @@
 const pool = require("../config/db");
 const Joi = require("joi");
+const scanValidationSchema = Joi.object({
+  label: Joi.string().max(100).required(),
+  confidence: Joi.number().min(0).max(1).required(),
+  status: Joi.string().max(50).optional().allow(null, ""),
+  plant: Joi.string().max(100).optional().allow(null, ""),
+  cause: Joi.string().max(255).optional().allow(null, ""),
+  symptoms: Joi.string().max(500).optional().allow(null, ""),
+  treatment: Joi.string().max(1000).optional().allow(null, ""),
+});
