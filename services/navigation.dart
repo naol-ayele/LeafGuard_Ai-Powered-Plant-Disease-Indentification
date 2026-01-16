fa54f0a297ad21c210efa050c77aea15a29a77c4
bedasa megarsa <bedasamegarsa9@gmail.com>
2026-01-16 13:56:57 -0800

    import required modules

---
+++ services/navigation.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/camera_screen.dart';
+import 'screens/history_screen.dart';
+
+import 'package:easy_localization/easy_localization.dart'; 
+
+import 'screens/SettingsScreen.dart'; 
