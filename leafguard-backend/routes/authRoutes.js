8015e4ac6c31f87742743e70ee7771ae22a54bc0
winnerJd <jossyastu@gmail.com>
2026-01-16 21:17:25 +0300

    add  auth routes

---
+++ leafguard-backend/routes/authRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.put("/change-password", authMiddleware, authController.changePassword);
+router.post("/forgot-password", authController.forgotPassword);
+router.post("/reset-password", authController.resetPassword);
+
+module.exports = router;
