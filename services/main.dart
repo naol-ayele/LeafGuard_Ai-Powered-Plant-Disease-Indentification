ba6f2a37d47f28c5ca293158db7671f1781ca396
bedasa megarsa <bedasamegarsa9@gmail.com>
2026-01-20 11:12:02 -0800

    main.dart import module

---
+++ services/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:easy_localization/easy_localization.dart'; // Added
+import 'screens/onboarding_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
+import 'navigation.dart';
+import 'ml/tflite_service.dart';
