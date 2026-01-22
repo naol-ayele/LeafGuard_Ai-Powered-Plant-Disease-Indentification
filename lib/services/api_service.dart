26449903988ac827be034d4e4739ffb3df692f14
bedasa megarsa <bedasamegarsa9@gmail.com>
2026-01-22 00:04:14 -0800

    feat(services): implement ApiService with uploadScan method and file upload

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
 import 'package:path/path.dart';
 class ApiService {
+  // Use your computer's IP address if testing on a real device
   static const String baseUrl =
       "https://subterraqueous-janis-heterothallic.ngrok-free.dev/api";
 
@@ -16,19 +17,21 @@ class ApiService {
     required String cause,
     required String symptoms,
     required String treatment,
-    required String token, 
+    required String token, // Required for your authMiddleware
   }) async {
     try {
       var uri = Uri.parse("$baseUrl/scans/upload");
       var request = http.MultipartRequest('POST', uri);
 
+      // 1. Add Auth Header
       request.headers['Authorization'] = 'Bearer $token';
       request.headers['ngrok-skip-browser-warning'] = 'true';
 
+      // 2. Add File
       var stream = http.ByteStream(imageFile.openRead());
       var length = await imageFile.length();
       var multipartFile = http.MultipartFile(
-        'image', 
+        'image', // Must match req.file name in Node.js
         stream,
         length,
         filename: basename(imageFile.path),
