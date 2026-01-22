73ffb294b62ce33332a3fad359ee18b36ac11619
winnerJd <jossyastu@gmail.com>
2026-01-22 11:56:42 +0300

    docs(auth): document register route

--- leafguard-backend/routes/authRoutes.js
+++ leafguard-backend/routes/authRoutes.js
@@ -8,8 +8,15 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const authMiddleware = require("../middleware/authMiddleware");
-
+/**
+ * Register a new user
+ *
+ * @route POST /register
+ * @access Public
+ */
 router.post("/register", authController.register);
+
+
 router.post("/login", authController.login);
 router.put("/change-password", authMiddleware, authController.changePassword);
 router.post("/forgot-password", authController.forgotPassword);
