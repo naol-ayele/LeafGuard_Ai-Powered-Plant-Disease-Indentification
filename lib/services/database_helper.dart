2d66ce60e4a1152a99ee931c69af7e80f6628949
bedasa megarsa <bedasamegarsa9@gmail.com>
2026-01-23 12:16:16 -0800

    Add lazy-initialized database getter in DatabaseHelper

--- lib/services/database_helper.dart
+++ lib/services/database_helper.dart
@@ -4,3 +4,8 @@ class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper._init();
   static Database? _database;
   DatabaseHelper._init();
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('leafguard.db');
+    return _database!;
+  }
