609b1b4c244d6b491de109df1d9602b40f269579
bedasa megarsa <bedasamegarsa9@gmail.com>
2026-01-23 12:52:47 -0800

    Add MainNavigation StatefulWidget with bottom navigation state

--- lib/services/navigation.dart
+++ lib/services/navigation.dart
@@ -5,3 +5,12 @@ import '../../services/screens/history_screen.dart';
 
 import 'package:easy_localization/easy_localization.dart';
 import '../../services/screens/SettingsScreen.dart';
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _currentIndex = 0;
