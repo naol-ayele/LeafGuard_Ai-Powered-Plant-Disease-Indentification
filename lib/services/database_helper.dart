0d371f86c5dd17294d7fb91946ad4076ad51361b
bedasa megarsa <bedasamegarsa9@gmail.com>
2026-01-23 12:14:52 -0800

    Initialize DatabaseHelper singleton for local database access

--- lib/services/database_helper.dart
+++ lib/services/database_helper.dart
@@ -1,2 +1,6 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+  DatabaseHelper._init();
