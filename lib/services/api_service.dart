044f7aa87889a19d9a11fc6718722d6754733d02
bedasa megarsa <bedasamegarsa9@gmail.com>
2026-01-23 11:28:41 -0800

    Add uploadScan API for sending scan results to backend

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -7,3 +7,19 @@ class ApiService {
   // Use your computer's IP address if testing on a real device
   static const String baseUrl =
       "https://subterraqueous-janis-heterothallic.ngrok-free.dev/api";
+Future<Map<String, dynamic>> uploadScan({
+    required File imageFile,
+    required String label,
+    required double confidence,
+    required String status,
+    required String plant,
+    required String cause,
+    required String symptoms,
+    required String treatment,
+    required String token, // Required for your authMiddleware
+  }) async {
+    try {
+      var uri = Uri.parse("$baseUrl/scans/upload");
+      var request = http.MultipartRequest('POST', uri);
+
+
