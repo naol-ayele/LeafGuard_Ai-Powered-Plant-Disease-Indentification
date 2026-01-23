7339430b386b23f44b59b6138f3ce4791f4f9a3e
bedasa megarsa <bedasamegarsa9@gmail.com>
2026-01-23 08:31:37 -0800

    Connect ApiService to backend API endpoint

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,3 +3,7 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
 import 'package:path/path.dart';
+class ApiService {
+  // Use your computer's IP address if testing on a real device
+  static const String baseUrl =
+      "https://subterraqueous-janis-heterothallic.ngrok-free.dev/api";
